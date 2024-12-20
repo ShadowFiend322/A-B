1b980f9ff02a50f27d7c515595c274e094edf583
ShadowFiend322 <Theschutzr@gmail.com>
2024-12-20 15:11:37 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+    int A = 0b101101;  
+    int B = 0b110011;  
+
+    int and_result = A & B;
+
+    int sum_result = A + B;
+
+    cout << "A AND B: " << bitset<6>(and_result) << endl;
+    cout << "Сумма A и B: " << bitset<7>(sum_result) << endl;  
+
+    return 0;
+}
